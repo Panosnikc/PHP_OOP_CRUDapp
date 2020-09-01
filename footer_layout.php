c2466d9daa4ac4bcd68e363da9e589d23fdd3212
Panosnikc <carouselpan@gmail.com>
2020-09-01 14:47:06 +0300

    footer

--- footer_layout.php
+++ footer_layout.php
@@ -2,7 +2,7 @@
 
 <!-- Latest jQuery -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
+ 
 <!-- Latest compiled and minified Bootstrap JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
